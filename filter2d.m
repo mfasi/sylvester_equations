d911daca49b509b2ccc6b68343a4b0e2b0158be4
Massimiliano Fasi <massimiliano.fasi@durham.ac.uk>
2022-08-17 20:36:24 +0100

    Fix reference for filter2d

--- filter2d.m
+++ filter2d.m
@@ -1,10 +1,9 @@
-function [A, B, C] = filter2D()
-  %
-  % [1] Benner & Saak (2005). A Semi-Discretized Heat Transfer Model for Optimal
-  %     Cooling of Steel Profiles. In: Benner, Sorensen, & Mehrmann (eds)
-  %     Dimension Reduction of Large-Scale Systems. Lecture Notes in Computational
-  %     Science and Engineering, vol 45. Springer, Berlin, Heidelberg.
-  %     https://doi.org/10.1007/3-540-27909-1_19
+function [A, B, C] = filter2d()
+%
+% [1] Hohlfeld, Bechtold, & Zappe (2005). Tunable Optical Filter. In: Benner,
+%     Sorensen, Mehrmann, (eds) Dimension Reduction of Large-Scale Systems.
+%     Lecture Notes in Computational Science and Engineering, vol 45. Springer,
+%     Berlin, Heidelberg. https://doi.org/10.1007/3-540-27909-1_15
 
   if ~exist('filter2D.mtx', 'file')
     unzip('https://nrvis.com/download/data/misc/filter2D.zip')
