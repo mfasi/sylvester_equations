70c54fb77239c78d497d2f0a7067ce70dba8eed7
Massimiliano Fasi <massimiliano.fasi@durham.ac.uk>
2022-08-31 09:44:41 +0100

    Add matrix sep_conv_diff_eq

--- am_properties.m
+++ am_properties.m
@@ -3,6 +3,7 @@ function P = am_properties
   P = {'ex_rand', {}
        'filter2D', {}
        'rail_1357', {}
+       'sep_conv_diff_eq', {}
        'slicot', {}
       };
 
---
+++ sep_conv_diff_eq.m
@@ -0,0 +1,32 @@
+function [coeff1, coeff2, rhs] = sep_conv_diff_eq(m, n, h, p1, p2, p3)
+  % Discretization of a boundary-value problem for a two-dimensional separable
+  % model convection-diffusion equation [1, sect. 7, pp. 306-307].
+  %
+  %  References:
+  %  [1] Hu, Reichel (1992) Krylov-Subspace Methods for the Sylvester Equation.
+  %      Linear Algebra Appl., 172:283–313.
+  %      https://doi.org/10.1016/0024-3795(92)90031-5
+
+  if nargin < 1
+    m = 100;
+  end
+  if nargin < 2
+    n = 100;
+  end
+  if nargin < 3
+    h = 0.001;
+  end
+  if nargin < 4
+    p1 = 0;
+  end
+  if nargin < 5
+    p2 = 0;
+  end
+  if nargin < 6
+    p3 = 0;
+  end
+
+  coeff1 = anymatrix('gallery/tridiag', m, -1-p1*h, 2-p3*h^2, -1+p1*h) / h^2;
+  coeff2 = -anymatrix('gallery/tridiag', n, -1-p2*h, 2-p3*h^2, -1+p2*h) / h^2;
+  rhs = [];
+end
